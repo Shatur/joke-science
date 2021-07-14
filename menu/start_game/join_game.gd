e099692e4d3add507755b72605b9c34b32cf0267
jara-gardaria <irina.kdr@gmail.com>
2021-07-14 13:17:22 +0300

    Add error mesage for join script

--- menu/start_game/join_game.gd
+++ menu/start_game/join_game.gd
@@ -6,7 +6,10 @@ onready var _ip_label: LineEdit = $GridContainer/IpLineEdit
 
 func _confirm() -> void:
 	var peer = NetworkedMultiplayerENet.new()
-	peer.create_client(_ip_label.text, _port_spinbox.value)
+	if peer.create_client(_ip_label.text, _port_spinbox.value) != OK:
+		_error_dialog.dialog_text = "Unable to join server"
+		_error_dialog.popup_centered()
+		return
+	
 	get_tree().network_peer = peer
-
 	._confirm()
