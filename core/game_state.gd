3caa0a861e40dc3efe4524a517944556fb36c63a
Hennadii Chernyshchyk <genaloner@gmail.com>
2021-07-15 21:43:52 +0300

    Validate player id

--- core/game_state.gd
+++ core/game_state.gd
@@ -5,6 +5,12 @@ var player_states: Array
 
 
 mastersync func add_player(nickname: String) -> void:
-	var player_state := PlayerState.new()
+	var id: int = get_tree().multiplayer.get_rpc_sender_id()
+	# Check if the id is already exists
+	for player_state in player_states:
+		if player_state.id == id:
+			get_tree().network_peer.disconnect_peer(id, true)
+
+	var player_state := PlayerState.new(id, nickname)
 	player_state.nickname = nickname
 	print("Connected: ", nickname)
--- core/player_state.gd
+++ core/player_state.gd
@@ -2,4 +2,10 @@ class_name PlayerState
 extends Node
 
 
+var id: int
 var nickname: String
+
+
+func _init(new_id: int, new_nickname: String) -> void:
+	id = new_id
+	nickname = new_nickname
