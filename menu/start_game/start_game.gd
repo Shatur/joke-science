7039de741448364bfc10cfdd5e7d7f2f621721dd
jara-gardaria <irina.kdr@gmail.com>
2021-07-19 14:45:39 +0300

    Add checking for empty nickname

--- menu/start_game/start_game.gd
+++ menu/start_game/start_game.gd
@@ -5,6 +5,7 @@ extends VBoxContainer
 onready var _nickname_edit: LineEdit = $GridContainer/NicknameEdit
 onready var _port_spinbox: SpinBox = $GridContainer/PortSpinBox
 onready var _error_dialog: AcceptDialog = $ErrorDialog
+onready var _confirm_button: Button = $ConfirmButton
 
 
 func _confirm():
@@ -15,3 +16,7 @@ func _enter_lobby() -> void:
 	GameState.rpc_id(0, "add_player", _nickname_edit.text)
 	# warning-ignore:return_value_discarded
 	get_tree().change_scene("res://core/board.tscn")
+
+
+func _on_nickname_changed(nickname: String) -> void:
+	_confirm_button.disabled = nickname.empty()
