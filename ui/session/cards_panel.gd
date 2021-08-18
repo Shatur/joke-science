ac4b7eeec5ad507488ab6a89a964f423dcb6c817
Hennadii Chernyshchyk <genaloner@gmail.com>
2021-08-18 23:00:57 +0300

    Add the ability to remove card

--- ui/session/cards_panel.gd
+++ ui/session/cards_panel.gd
@@ -1,7 +1,8 @@
 extends Panel
 
 
-signal word_choosen(word)
+signal word_added(word)
+signal word_removed(word)
 
 const CardButtonScene: PackedScene = preload("res://ui/session/card_button.tscn")
 
@@ -16,21 +17,24 @@ func _ready():
 func redraw_cards(substitution_index: int) -> void:
 	var substitutions: Array = GameState.current_sentence["substitutions"]
 	if substitutions.size() == substitution_index:
-		_disable_all_cards()
+		_disable_unpressed_cards()
 	else:
 		_detect_available_words(substitutions[substitution_index])
 
 
-func _disable_all_cards() -> void:
+func _disable_unpressed_cards() -> void:
 	for card_button in _grid_container.get_children():
-		card_button.disabled = true
+		if not card_button.pressed:
+			card_button.disabled = true
 
 
 func _detect_available_words(substitution: Dictionary) -> void:
 	var case: String = substitution["case"]
 	var number = substitution.get("number")
 	for card_button in _grid_container.get_children():
-		card_button.detect_available_words(case, number)
+		if not card_button.pressed:
+			card_button.detect_available_words(case, number)
+			card_button.disabled = false
 
 
 func _add_card(card: Dictionary) -> void:
@@ -38,8 +42,11 @@ func _add_card(card: Dictionary) -> void:
 	_grid_container.add_child(card_button)
 	card_button.card = card
 	# warning-ignore:return_value_discarded
-	card_button.connect("pressed", self, "_on_card_selected", [card_button])
+	card_button.connect("toggled", self, "_on_card_toggled", [card_button])
 
 
-func _on_card_selected(card_button: CardButton) -> void:
-	emit_signal("word_choosen", card_button.label.text)
+func _on_card_toggled(button_pressed: bool, card_button: CardButton) -> void:
+	if button_pressed:
+		emit_signal("word_added", card_button.label.text)
+	else:
+		emit_signal("word_removed", card_button.label.text)
--- ui/session/sentence_label.gd
+++ ui/session/sentence_label.gd
@@ -6,7 +6,13 @@ signal substitution_index_changed(index)
 
 var sentence: String setget set_sentence
 
+var _regex := RegEx.new()
 var _words: Dictionary
+
+
+func _init() -> void:
+	# warning-ignore:return_value_discarded
+	_regex.compile("\\{\\d\\}")
 
 
 func set_sentence(new_sentence: String) -> void:
@@ -17,23 +23,31 @@ func set_sentence(new_sentence: String) -> void:
 
 
 func add_word(word: String) -> void:
-	var index: int = 0
-	while _words.has(index):
-		index += 1
-
+	var index: int = _first_missing_index()
 	_words[index] = word
 	_redraw_text()
-	emit_signal("substitution_index_changed", _words.size())
+	emit_signal("substitution_index_changed", _first_missing_index(index))
 
 
 func remove_word(word: String) -> void:
-	var index = _words.get(word)
-	assert(index != null, "Unable to remove word from sentence label: " + word)
+	var remove_index: int = -1
+	for index in _words:
+		if _words[index] == word:
+			remove_index = index
+	
+	assert(remove_index != -1, "Unable to remove word from sentence label: " + word)
 	# warning-ignore:return_value_discarded
-	_words.erase(index)
+	_words.erase(remove_index)
 	_redraw_text()
-	emit_signal("substitution_index_changed", _words.size())
+	emit_signal("substitution_index_changed", _first_missing_index())
 
 
 func _redraw_text() -> void:
-	bbcode_text = sentence.format(_words.values(), "...")
+	bbcode_text = _regex.sub(sentence.format(_words), "...", true)
+
+
+func _first_missing_index(begin: int = 0) -> int:
+	for index in range(begin, _words.size()):
+		if not _words.has(index):
+			return index
+	return _words.size()
